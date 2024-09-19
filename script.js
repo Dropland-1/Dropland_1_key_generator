8f7a886653e0e2b872bd590727fccd1c00e6088e
Dropland-1 <hawdi.apex@gmail.com>
2024-09-19 14:56:39 +0330

    Update script.js

--- script.js
+++ script.js
@@ -88,6 +88,34 @@ document.addEventListener('DOMContentLoaded', () => {
             eventsDelay: 40000,
             attemptsNumber: 30,
 	}
+	13: {
+            name: 'Pin Out Master',
+            appToken: 'd2378baf-d617-417a-9d99-d685824335f0',
+            promoId: 'd2378baf-d617-417a-9d99-d685824335f0',
+            eventsDelay: 20000,
+            attemptsNumber: 30,
+        },
+        14: {
+            name: 'Count Masters',
+            appToken: '4bdc17da-2601-449b-948e-f8c7bd376553',
+            promoId: '4bdc17da-2601-449b-948e-f8c7bd376553',
+            eventsDelay: 20000,
+            attemptsNumber: 30,
+	}
+	15: {
+            name: 'Infected Frontier',
+            appToken: 'eb518c4b-e448-4065-9d33-06f3039f0fcb',
+            promoId: 'eb518c4b-e448-4065-9d33-06f3039f0fcb',
+            eventsDelay: 20000,
+            attemptsNumber: 30,
+        },
+        16: {
+            name: 'Among Water',
+            appToken: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
+            promoId: 'daab8f83-8ea2-4ad0-8dd5-d33363129640',
+            eventsDelay: 20000,
+            attemptsNumber: 30,
+	}
     };
 
 
